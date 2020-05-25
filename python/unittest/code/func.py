ea455ba52302dd0b2f83cbf6f1de68033230f38b
Dino S Zhang/WZS/Wistron <Dino_S_Zhang@wistron.com>
2020-05-25 18:20:36 +0800

    unittest note

---
+++ python/unittest/code/func.py
@@ -0,0 +1,15 @@
+
+
+def sort_lis(lis, sort_key, desc):
+    # sort
+    if len(sort_key):
+        if sort_key != 'Location':
+            lis = sorted(lis, key=lambda x: x[sort_key])
+        else:
+            lis = sorted(lis, key=lambda x: (x['Rack'],x['Bay'],x['Inline']))
+        if desc:
+            lis.reverse()
+    else:
+        sort_key = 'Location'   # default
+        lis = sorted(lis, key=lambda x: (x['Rack'],x['Bay'],x['Inline']))
+    return lis
